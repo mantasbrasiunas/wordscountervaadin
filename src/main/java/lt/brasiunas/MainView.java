9e5b17508f05b06788396a8157f2d6bf627cdfe7
Mantas Brasiunas <mantas@brasiunas.lt>
2021-10-04 12:03:02 +0300

    Sukurtas karkasas.

--- src/main/java/lt/brasiunas/MainView.java
+++ src/main/java/lt/brasiunas/MainView.java
@@ -1,19 +1,43 @@
 package lt.brasiunas;
 
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextArea;
+import com.vaadin.flow.component.upload.Upload;
 import com.vaadin.flow.router.Route;
 
-/**
- * The main view contains a button and a click listener.
- */
+import java.util.ArrayList;
+
 @Route
 public class MainView extends VerticalLayout {
 
     public MainView() {
-        Button button = new Button("Click me",
-                event -> Notification.show("Clicked!"));
-        add(button);
+        Upload upload = new Upload();
+        Button btnBegin = new Button("Begin counting");
+        btnBegin.setEnabled(false);
+        
+        Button btnDownloadAll = new Button("Download all");
+        btnDownloadAll.setEnabled(false);
+        
+        add(upload, btnBegin, btnDownloadAll);
+        
+        ArrayList<TextArea> txasWords = new ArrayList<>();
+        ArrayList<Button> btnsDownload = new ArrayList<>();
+        String[] firstLetters = { "A-G", "H-N", "O-U", "V-Z" };
+        for (int i = 0; i < 4; i++) {
+			TextArea txaWords = new TextArea("Words frequency " + 
+				firstLetters[i]);
+			txaWords.setWidth("100%");
+			txaWords.setHeight("250px");
+			txasWords.add(txaWords);
+			
+			Button btnDownload = new Button("Download file");
+			btnDownload.setEnabled(false);
+			btnsDownload.add(btnDownload);
+			
+			add(txaWords, btnDownload);
+		}
     }
 }
